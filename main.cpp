09163eb2de81f511a4bb61b13e0633ec63685933
PetitLucien <lucienpetit91@outlook.com>
2024-11-28 14:20:25 +0000

    adding distance capteur code

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include "mbed.h"
 #include "string.h"
+
 // Create a DigitalOutput object to toggle an LED whenever data is received.
 static DigitalOut led(LED1);
-
+AnalogIn   capteur(A0);
 // Create a UnbufferedSerial object with a default baud rate.
 static UnbufferedSerial serial_port(PC_1, PC_0);
 static UnbufferedSerial pc(USBTX, USBRX,9600);
 
-string guess;
+//string guess;
 
 void on_rx_interrupt()
 {    char c;
@@ -18,15 +19,15 @@ void on_rx_interrupt()
     // Read the data to clear the receive interrupt.
     if (serial_port.read(&c, 1)) {
         // Echo the input back to the terminal.
-        pc.write(&guess, guess.size());
+       // pc.write(&guess, guess.size());
         switch(c) {
                 case 0xE1 :
                     pc.write("1", 1);
-                    guess.append("1");
+                    //guess.append("1");
                     break;
                 case 0xE2 :
                     pc.write("2", 1);
-                    guess.append("2");
+                    //guess.append("2");
                     break;
                 case 0xE3 :
                     pc.write("3", 1);
@@ -77,4 +78,20 @@ int main(void)
 
     // Register a callback to process a Rx (receive) interrupt.
     serial_port.attach(&on_rx_interrupt, SerialBase::RxIrq);
+
+        while (1) {
+        // test the voltage on the initialized analog pin
+        //  and if greater than 0.3 * VCC set the digital pin
+        //  to a logic 1 otherwise a logic 0
+        if (capteur > 0.3f) {
+            led = 1;
+        } else {
+            led = 0;
+        }
+
+        // print the percentage and 16 bit normalized values
+        printf("percentage: %3.3f%%\n", capteur.read() * 100.0f);
+        printf("normalized: %d \n", int(capteur.read_u16()*0.01998425));
+        ThisThread::sleep_for(200ms);
+    }
 }
